08b5999738afebecd1bc172c3602352cd7cad517
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-09 23:26:50 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 
-# class HauntedHouses < ActiveRecord::Migration
+# class HauntedHouses < ActiveRecord::Migration [4.2]
   
 #   def change
 #     create_table :haunted_houses do |t|
