ecae1e90a4b5498eff01bdba190d98f2aad9d82d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-09 18:56:50 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+# create CostumeStores < ActiveRecord::Migration
+
+#   def change
+#     create_table :costume_stores |t|
+#       t.string :name
+#       t.string :location
+#       t.integer :costume_inventory
+#       t.integer :num_of_employees
+#       t.string :still_in_business
+#       t.datetime :opening_time
+#       t.datetime :closing_time
+#     end
+# end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,19 @@
 # Create your haunted_houses migration here
+
+# def HauntedHouses < ActiveRecord::Migration
+  
+#   def change
+#     create_table :haunted_houses do |t|
+#       t.string :name
+#       t.string :location
+#       t.string :theme
+#       t.integer :price
+#       t.string :family_friendly
+#       t.datetime :opening_date
+#       t.datetime :closing_date
+#       t.string :description
+#     end
+    
+#   end
+  
+# end
