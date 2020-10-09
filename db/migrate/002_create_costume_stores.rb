dfd1d4578a0bcebb01b77aeeb747ef348d2035c8
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-09 23:34:13 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,15 +1,16 @@
 # Create your costume_stores migration here
 
-# class CostumeStores < ActiveRecord::Migration[5.1]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
-#   def change
-#     create_table :costume_stores |t| do
-#       t.string :name
-#       t.string :location
-#       t.integer :costume_inventory
-#       t.integer :num_of_employees
-#       t.string :still_in_business
-#       t.datetime :opening_time
-#       t.datetime :closing_time
-#     end
-# end
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+end
