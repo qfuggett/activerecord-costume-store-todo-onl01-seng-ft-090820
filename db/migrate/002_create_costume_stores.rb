93551afb0ab41c3c5e788585f348d942c4de6ebf
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-09 23:26:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@
 # class CostumeStores < ActiveRecord::Migration[4.2]
 
 #   def change
-#     create_table :costume_stores |t| do
+#     create_table :costume_stores do |t|
 #       t.string :name
 #       t.string :location
 #       t.integer :costume_inventory
